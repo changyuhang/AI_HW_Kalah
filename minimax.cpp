da82a63295a8368ef57bd5d3ac2000887603e414
張語航 <00257122@ntou.edu.tw>
2017-05-12 20:56:31 +0800

    alpha beta pruning

--- minimax.cpp
+++ minimax.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 enum players { human = false, computer = true};
 const int player_store[2] = {6, 13};
 
-int minimaxDecision(int house[], int depthMAX);
-void maxValue(int house[], int depth, int depthMAX, int *mValue, int *action);
-void minValue(int house[], int depth, int depthMAX, int *mValue, int *action);
+int c = 0;
+
+int minimaxDecision(int house[], int depth);
+int maxValue(int house[], int depth, int alpha, int beta, int *action);
+int minValue(int house[], int depth, int alpha, int beta, int *action);
 
 void PrintHouse(int house[]){
     for (size_t i = 0; i < 14; i++) {
@@ -59,88 +62,93 @@ bool relocation(int now_house[], int picked_house, int next_house[]){
 }
 
 
-int minimaxDecision(int house[], int depthMAX) {
-    int action;
-    int v;
-    maxValue(house, 0 ,depthMAX, &v, &action);
+int minimaxDecision(int house[], int depth) {
+    int action = -1;
+    //int v;
+    int alpha = -numeric_limits<int>::max();
+    int beta = numeric_limits<int>::max();
+    maxValue(house, depth, alpha, beta, &action);
     return action;
 }
 
-void maxValue(int house[], int depth, int depthMAX, int *mValue, int *action) {
-    if (depth == depthMAX) {
-        *mValue = evaluate(house);
+int maxValue(int house[], int depth, int alpha, int beta, int *action) {
+    c++;
+    if (depth == 0) {
+        return evaluate(house);
         //PrintHouse(house);
-        return ;
     }
     int successor[6];
     int possible_actions = listSuccessors(house, successor, computer);
-    int v = -99999;
     if (possible_actions == 0) {
-        *mValue = evaluate(house);
+        return evaluate(house);
         //PrintHouse(house);
-        return ;
     }
     for (size_t i = 0; i < possible_actions; i++) {
         int next_house[14];
         int value, a;
         bool more_action = relocation(house, successor[i], next_house);
         if (more_action) {
-            maxValue(next_house, depth + 1, depthMAX, &value, &a);
+            value = maxValue(next_house, depth - 1, alpha, beta, &a);
         }else {
-            minValue(next_house, depth + 1, depthMAX, &value, &a);
+            value = minValue(next_house, depth - 1, alpha, beta, &a);
         }
-        if (v < value) {
-            v = value;
+        if (alpha < value) {
+            alpha = value;
             *action = successor[i];
         }
+        if (beta <= alpha) {
+            return beta;
+        }
     }
-    *mValue = v;
-    return ;
+    return alpha;
 }
 
-void minValue(int house[], int depth, int depthMAX, int *mValue, int *action) {
-    if (depth == depthMAX) {
-        *mValue = evaluate(house);
+int minValue(int house[], int depth, int alpha, int beta, int *action) {
+    c++;
+    if (depth == 0) {
+        return evaluate(house);
         //PrintHouse(house);
-        return ;
     }
     int successor[6];
     int possible_actions = listSuccessors(house, successor, human);
-    int v = 99999;
 
     if (possible_actions == 0) {
-        *mValue = evaluate(house);
+        return evaluate(house);
         //PrintHouse(house);
-        return ;
     }
     for (size_t i = 0; i < possible_actions; i++) {
         int next_house[14];
         int value, a;
         bool more_action = relocation(house, successor[i], next_house);
         if (more_action) {
-            minValue(next_house, depth + 1, depthMAX, &value, &a);
+            value = minValue(next_house, depth - 1, alpha, beta, &a);
         }else {
-            maxValue(next_house, depth + 1, depthMAX, &value, &a);
+            value = maxValue(next_house, depth - 1, alpha, beta, &a);
         }
-        if (v > value) {
-            v = value;
+        if (beta > value) {
+            beta = value;
             *action = successor[i];
         }
+        if (beta <= alpha) {
+            return beta;
+        }
     }
-    *mValue = v;
+    return beta;
 }
 
 void test_minimax() {
     int ts;
     cin >> ts;
     while (ts--) {
+        c = 0;
         int house[14];
-        int depthMAX;
+        int depth;
         for (size_t i = 0; i < 14; i++) {
             cin >> house[i];
         }
-        cin >> depthMAX;
-        cout << minimaxDecision(house, depthMAX) << endl;
+        cin >> depth;
+        cout << minimaxDecision(house, depth) << endl;
+        cout << "深度" << depth << "次數" << c << endl;
     }
     return ;
 }
